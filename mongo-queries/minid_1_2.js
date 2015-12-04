04266a23c262bdbe6c0c7fde515ac0b2ba62f83d
Andrea Masi <eraclitux@gmail.com>
2015-12-04 18:02:48 +0100

    Add multiple institutions with "governo"

--- mongo-queries/minid_1_2.js
+++ mongo-queries/minid_1_2.js
@@ -27,6 +27,10 @@ result = db['all'].aggregate(
     },
 		{ $sort: {"_id.gruppo":-1, "_id.indice_completezza": 1}}
 );
+print( "gruppo", ",", "indice_completezza", ",", "totale");
+result.forEach( function(i) {
+  print( i._id.gruppo, ",", i._id.indice_completezza, ",", i.count);
+});
 
 // debug
 result = db['all'].aggregate(
@@ -69,7 +73,7 @@ result.forEach( function(i) {
           print( i._id.partito, ",", i._id.indice_completezza, ",", i.count);
 });
 
-// chi sono i tecnici
+// chi sono i tecnici (partito acronym vuoto)
 result = db['all'].aggregate(
 		{ $match: {"anno_dichiarazione": 2014}},
 		{ $unwind: "$incarichi"},
@@ -79,7 +83,48 @@ result = db['all'].aggregate(
 
 
 print("chi ha acronimo partito vuoto");
-print( "partito", ",", "indice_completezza", ",", "totale");
 result.forEach( function(i) {
           print( i.op_id, ",", i.nome, ",", i.cognome);
+});
+
+result = db['all'].aggregate(
+		{ $match: {"anno_dichiarazione": 2014}},
+		{ $unwind: "$incarichi"},
+		{ $group: {
+                _id : { op_id:"$op_id", istituzione: "$incarichi.istituzione" },
+              }
+    },
+		{ $match: { "_id.istituzione": { $ne: "camera"}}},
+		{ $group: {
+                _id : { op_id:"$_id.op_id"},
+               count: { $sum: 1},
+              }
+    },
+		{ $match: { "count": { $gt: 1}}}
+);
+
+print("chi sta al governo ed in senato");
+result.forEach( function(i) {
+          print( i._id.op_id, i.count );
+});
+
+result = db['all'].aggregate(
+		{ $match: {"anno_dichiarazione": 2014}},
+		{ $unwind: "$incarichi"},
+		{ $group: {
+                _id : { op_id:"$op_id", istituzione: "$incarichi.istituzione" },
+              }
+    },
+		{ $match: { "_id.istituzione": { $ne: "senato"}}},
+		{ $group: {
+                _id : { op_id:"$_id.op_id"},
+               count: { $sum: 1},
+              }
+    },
+		{ $match: { "count": { $gt: 1}}}
+);
+
+print("chi sta al governo ed alla camera");
+result.forEach( function(i) {
+          print( i._id.op_id, i.count );
 });
