c372675d946c31dc775a19b4ea1ed3aecee84ff6
Andrea Masi <eraclitux@gmail.com>
2015-12-10 12:53:20 +0100

    Fix lazy images load scroll bug

--- html/frontend/javascript/controllers/list-controllers.js
+++ html/frontend/javascript/controllers/list-controllers.js
@@ -2,14 +2,17 @@ angular.module('PatrimoniTrasparenti')
   .controller('ListController', ['$scope', '$filter', 'Declarations', function($scope, $filter, Declarations){
     console.log('ctrl start');
     $scope.orderK = "cognome";
-    // this function is no more used
     $scope.selectOrder = function(k) {
+    // this function is no more used
       // little hack to fire "scroll" event
       // for lazy images loading.
       window.scrollBy(0, 1);
       $scope.orderK = k;
       console.log('ctrl orderK', $scope.orderK);
     };
+    $scope.$watch('orderK', function() {
+      window.scrollBy(0, 1);
+    });
     $scope.spinner = true;
 
     Declarations.getBy($scope.type, $scope.key)
