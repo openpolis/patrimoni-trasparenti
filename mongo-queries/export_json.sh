1d83237a4eb829129c2c8915323b303c95d69b4e
Andrea Masi <eraclitux@gmail.com>
2016-01-04 07:54:54 +0100

    Fix export_json.sh

--- mongo-queries/export_json.sh
+++ mongo-queries/export_json.sh
@@ -3,7 +3,7 @@
 years="2013 2014 2015";
 
 Export () {
-	mongoexport -d declarations -c all --type=csv \
+	mongoexport -d declarations -c all \
 		-q "{\"anno_dichiarazione\":${1}}"\
 		--type=json --jsonArray \
 		--out ${1}.json
