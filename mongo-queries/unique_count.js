6ccbf210b69f4e66691642125adfa8fc47e63fca
Andrea Masi <eraclitux@gmail.com>
2016-01-13 11:34:28 +0100

    Add verification query

--- mongo-queries/unique_count.js
+++ mongo-queries/unique_count.js
@@ -1,6 +1,6 @@
 // from a shell run:
 // mongo localhost/declarations unique_count.js
-var data = db['parliamentarians'].find();
+var data = db['all'].find();
 
 print("######################################")
 print( "# Calculate number of uniqe values for some fields");
@@ -11,7 +11,7 @@ print("######################################")
 print("######################################")
 print("# comune_residenza")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{"$group": { _id : "$comune_residenza", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
 );
@@ -22,7 +22,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# beni_immobili.descrizione")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$beni_immobili"},
 		{"$group": { _id : "$beni_immobili.descrizione", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -34,7 +34,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# beni_immobili.natura_diritto")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$beni_immobili"},
 		{"$group": { _id : "$beni_immobili.natura_diritto", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -46,7 +46,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# beni_mobili.tipologia")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$beni_mobili"},
 		{"$group": { _id : "$beni_mobili.tipologia", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -57,7 +57,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# beni_mobili.anno_immatricolazione")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$beni_mobili"},
 		{"$group": { _id : "$beni_mobili.anno_immatricolazione", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -68,7 +68,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# beni_mobili.cavalli_fiscali")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$beni_mobili"},
 		{"$group": { _id : "$beni_mobili.cavalli_fiscali", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -79,7 +79,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# partecipazioni.denominazione")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$partecipazioni_soc"},
 		{"$group": { _id : "$partecipazioni_soc.denominazione", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -90,7 +90,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# partecipazioni.numero_azioni_quote")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$partecipazioni_soc"},
 		{"$group": { _id : "$partecipazioni_soc.numero_azioni_quote", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -101,7 +101,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# amministrazioni_soc.denominazione")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$amministrazioni_soc"},
 		{"$group": { _id : "$amministrazioni_soc.denominazione", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -112,7 +112,7 @@ result.forEach( function(i) {
 print("######################################")
 print("# amministrazioni_soc.natura_incarico")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$amministrazioni_soc"},
 		{"$group": { _id : "$amministrazioni_soc.natura_incarico", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
@@ -123,11 +123,48 @@ result.forEach( function(i) {
 print("######################################")
 print("# contributi_elettorali.anno")
 print("######################################")
-result = db['parliamentarians'].aggregate(
+result = db['all'].aggregate(
 		{$unwind: "$contributi_elettorali"},
 		{"$group": { _id : "$contributi_elettorali.anno", 'total': { $sum: 1}}},
 		{"$sort": {'total':-1}}
 );
 result.forEach( function(i) {
 	print( tojson( i, "", true) + "," );
+});
+
+print("######################################")
+print("# contributi_elettorali.fonte")
+print("######################################")
+result = db['all'].aggregate(
+		{$unwind: "$contributi_elettorali"},
+		{"$group": { _id : "$contributi_elettorali.fonte", 'total': { $sum: 1}}},
+		{"$sort": {'total':-1}}
+);
+result.forEach( function(i) {
+	print( tojson( i, "", true) + "," );
+});
+print("# contributi_elettorali.fonte , chi sono con maiuscole")
+result = db['all'].aggregate(
+		{"$match": {"contributi_elettorali.fonte": 'contibuti o servizi ricevuti dal partito' }}
+);
+result.forEach( function(i) {
+	print(  i.op_id );
+});
+print("######################################")
+print("# spese_elettorali.fonte")
+print("######################################")
+result = db['all'].aggregate(
+		{$unwind: "$spese_elettorali"},
+		{"$group": { _id : "$spese_elettorali.fonte", 'total': { $sum: 1}}},
+		{"$sort": {'total':-1}}
+);
+result.forEach( function(i) {
+	print( tojson( i, "", true) + "," );
+});
+print("# spese_elettorali.fonte , chi sono con maiuscole")
+result = db['all'].aggregate(
+		{"$match": {"spese_elettorali.fonte": 'Spese sostenute dal partito' }}
+);
+result.forEach( function(i) {
+	print(  i.op_id );
 });
