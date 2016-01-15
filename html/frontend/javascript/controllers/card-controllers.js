86ebe86fbc7714fb23a10ff9e2d28fc552f53881
Andrea Masi <eraclitux@gmail.com>
2016-01-15 11:56:36 +0100

    Show link if zero political incomes/outcomes
    Close #35

--- html/frontend/javascript/controllers/card-controllers.js
+++ html/frontend/javascript/controllers/card-controllers.js
@@ -11,8 +11,49 @@ angular.module('PatrimoniTrasparenti')
       //reset to old to keep any additional routing logic from kicking in
       $location.hash(old);
     };
+    $scope.selectAndScroll = function(i, id) {
+      if (i === -1) {
+        // nothing to show, do nothing
+        return
+      };
+      $scope.selectYear(i);
+      // FIXME this call has no effect
+      $scope.scrollTo(id);
+    };
     $scope.isNumber = function(value) {
       return angular.isNumber(value);
+    };
+    $scope.getLastSpeseYear = function() {
+      $scope.lastSpeseYearIndex = -1;
+      $scope.lastSpeseYear = 0;
+      for (var i in $scope.schede) {
+        scheda = $scope.schede[i];
+        if (scheda["totale_spese_elettorali"] != 0) {
+          $scope.lastSpeseYear = scheda["anno_dichiarazione"];
+          $scope.lastSpeseYearIndex = i;
+        };
+      };
+      if ($scope.lastSpeseYear != 0) {
+        return "Vedi anno " + $scope.lastSpeseYear;
+      } else {
+        return "Nessuna voce";
+      }
+    };
+    $scope.getLastContributiYear = function() {
+      $scope.lastContributiYearIndex = -1;
+      $scope.lastContributiYear = 0;
+      for (var i in $scope.schede) {
+        scheda = $scope.schede[i];
+        if (scheda["totale_contributi_elettorali"] != 0) {
+          $scope.lastContributiYear = scheda["anno_dichiarazione"];
+          $scope.lastContributiYearIndex = i;
+        };
+      };
+      if ($scope.lastContributiYear != 0) {
+        return "Vedi anno " + $scope.lastContributiYear;
+      } else {
+        return "Nessuna voce";
+      }
     };
     Declarations.getFor($scope.opId)
       .success(function(schede){
