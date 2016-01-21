3424a490692649a4dffaaaca55f0695f866cfdc8
Andrea Masi <eraclitux@gmail.com>
2016-01-21 14:09:05 +0100

    Add icon to popover
    Close #28 close #31

--- html/frontend/javascript/directives/popover.js
+++ html/frontend/javascript/directives/popover.js
@@ -2,7 +2,7 @@ angular.module("PatrimoniTrasparenti")
   .directive('customPopover', function () {
       return {
           restrict: 'A',
-          template: '<span>{{label}}</span>',
+          template: '<span><img src="assets/images/info.png" alt="{{label}}" width="16" height="16" data-src2x="assets/images/info@2x.png"></span>',
           link: function (scope, el, attrs) {
               scope.label = attrs.popoverLabel;
 
