6a1d8decabb74cfb9396d153d8cf1ca8243cf55d
Andrea Masi <eraclitux@gmail.com>
2016-01-25 09:44:35 +0100

    Close popover when clicked anywhere in page
    Close #57

--- html/frontend/javascript/directives/popover.js
+++ html/frontend/javascript/directives/popover.js
@@ -1,18 +1,29 @@
 angular.module("PatrimoniTrasparenti")
-  .directive('customPopover', function () {
+  .directive('customPopover', ["$timeout", function ($timeout) {
       return {
           restrict: 'A',
           template: '<span><img src="assets/images/info.png" alt="{{label}}" width="16" height="16" data-src2x="assets/images/info@2x.png"></span>',
           link: function (scope, el, attrs) {
               scope.label = attrs.popoverLabel;
-
-              $(el).popover({
-                  trigger: 'click',
-                  html: true,
-                  content: attrs.popoverHtml,
-                  title: attrs.popoverTitle,
-                  placement: attrs.popoverPlacement
+              $timeout(function(){
+                $(el).popover({
+                    trigger: 'click',
+                    html: true,
+                    content: attrs.popoverHtml,
+                    title: attrs.popoverTitle,
+                    placement: attrs.popoverPlacement
+                });
+                // Close popover when click everywhere on
+                // the page.
+                // FIXME this introduce "double click to open popoover" bug
+                $('html').on('mouseup', function(e) {
+                    if(!$(e.target).closest('.popover').length) {
+                        $('.popover').each(function(){
+                            $(this.previousSibling).popover('hide');
+                        });
+                    }
+                });
               });
           }
       };
-  });
+  }]);
