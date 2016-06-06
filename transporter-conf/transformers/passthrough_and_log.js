2fa96b98d9de852fc4049907a1e9e7d014e46755
Guglielmo Celata <guglielmo.celata@gmail.com>
2016-06-06 16:19:00 +0200

    transporter config changed

--- transporter-conf/transformers/passthrough_and_log.js
+++ transporter-conf/transformers/passthrough_and_log.js
@@ -1,4 +1,7 @@
 module.exports = function(doc) {
-  console.log("transformer: " + JSON.stringify(doc))
+  doc.data.id = doc.data._id['$oid']
+  delete doc.data._id
+  console.log("data: " + JSON.stringify(doc.data.id))
+  // console.log("transformer: " + JSON.stringify(doc))
   return doc
 }
